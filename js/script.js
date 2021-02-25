4dce8947de6487763fb0477ad1a657c73dd8cf16
joceilton <joceilton.g@gmail.com>
2021-02-25 14:22:37 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,5 +3,9 @@ $(function() {
 	$(".navbar-toggler").on("click", function() {
 		$(".menu-mobile").slideToggle()
 	})
+
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
 	
 })
